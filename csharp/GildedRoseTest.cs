b8d5b5a8eeb3a8f978c6ffad71f15062b0e88e71
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-16 14:13:54 +0100

    quality not negative or equal test passed.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -14,5 +14,14 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("fixme", Items[0].Name);
         }
+
+        [Test]
+        public void QualityNotNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item() { Name = "foo", SellIn = 0, Quality = 5} };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 0);
+        }
     }
 }
