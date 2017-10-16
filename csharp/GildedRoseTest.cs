2eb1c0c3cda6a67a779e1d808551be84db700b6e
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-16 15:56:59 +0100

    added ordinary degrade test for monitoring

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -27,7 +27,7 @@ namespace csharp
         [Test]
         public void QualityNotOverFifty()
         {
-            IList<Item> Items = new List<Item> { new Item() { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item() { Name = "foo", SellIn = 0, Quality = 50 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.LessOrEqual(Items[0].Quality, 50);
@@ -55,7 +55,7 @@ namespace csharp
             IList<Item> Items = new List<Item> { new Item() { Name = "random", SellIn = -1, Quality = 10 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            if (Items[0].Name != "Aged Brie" && Items[0].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <0)
+            if (Items[0].Name != "Aged Brie" && Items[0].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <0 && Items[0].Name != "Sulfuras, Hand of Ragnaros")
                 Assert.AreEqual(8, Items[0].Quality);
         }
 
@@ -84,9 +84,9 @@ namespace csharp
 
             if (Items[0].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 10 && Items[0].SellIn >= 6)
                 Assert.AreEqual(12, Items[0].Quality);
-            if (Items[0].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 5 && Items[0].SellIn >= 1)
+            if (Items[1].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 5 && Items[0].SellIn >= 1)
                 Assert.AreEqual(Items[1].Quality, 13);
-            if (Items[0].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 0)
+            if (Items[2].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 0)
                 Assert.AreEqual(0, Items[2].Quality);
         }
         [Test]
@@ -94,19 +94,29 @@ namespace csharp
         {
             IList<Item> Items = new List<Item>
             {
-                new Item() {Name = "Conjured", SellIn = 8, Quality = 10},
-                new Item() {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 3, Quality = 10},
+                new Item() {Name = "Conjured Mana Cake", SellIn = 0, Quality = 10},
+                new Item() {Name = "Conjured Mana Cake", SellIn = 3, Quality = 10},
             };
 
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
 
-            if (Items[0].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 10 && Items[0].SellIn >= 6)
-                Assert.AreEqual(12, Items[0].Quality);
-            if (Items[0].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[0].SellIn <= 5 && Items[0].SellIn >= 1)
-                Assert.AreEqual(Items[1].Quality, 13);
-
+            if (Items[0].Name == "Conjured Mana Cake" && Items[0].SellIn <= 0)
+                Assert.AreEqual(6, Items[0].Quality);
+            if (Items[1].Name == "Conjured Mana Cake" && Items[0].SellIn >= 1)
+                Assert.AreEqual(Items[1].Quality, 8);
         }
 
+        [Test]
+        public void OrdinaryDegrade()
+        {
+            IList<Item> Items = new List<Item> { new Item() { Name = "", SellIn = 3, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            if (Items[0].Name != "Aged Brie" && Items[0].Name != "Backstage passes to a TAFKAL80ETC concert" && 
+                Items[0].Name != "Sulfuras, Hand of Ragnaros" &&
+                Items[0].SellIn < 0)
+                Assert.AreEqual(9, Items[0].Quality);
+        }
     }
 }
